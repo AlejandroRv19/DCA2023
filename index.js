e3d8c10608a8bc45423b63abf57954b46f6b1c75
AlejandroRv19 <98325607+AlejandroRv19@users.noreply.github.com>
2023-02-07 09:07:55 -0500

    quiz 2023

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-const count = (n) => {
-    for (let index = 0; index < array.length; index++) {
+const numero = (n) => {
+    for (let i = 0; index < array.length; i++) {
         const element = array[index];
-        const isBy3 = index % 3 === 0;
-        const isBy5 = index % 5 === 0;
+        const isBy3 = i % 3 === 0;
+        const isBy5 = i % 5 === 0;
         
 
         if (isBy3 && isBy5) {
 
-            console.log(i + " by both");
+            console.log( i + " by both")
             
-            } else if (isBy5) {
-            
-            console.log(i + " by 5");
-            
-            } else if (isBy3) {
-            
-            console.log(i + " by 3");
-            
-            } else {
-            
-            console.log(i);
+        }
     }
-}
 
 }
-count(15); 

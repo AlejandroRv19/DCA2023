4a9105578b657a6ad072ae08a4436bc861832a00
AlejandroRv19 <98325607+AlejandroRv19@users.noreply.github.com>
2023-02-07 08:40:30 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-var numero = 1
-numero ++;
+const count = (n) => {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        const isBy3 = index % 3 === 0;
+        const isBy5 = index % 5 === 0;
+        
 
+        if (isBy3 && isBy5) {
 
-    console.log(numero)
+            console.log(i + " by both");
+            
+            } else if (isBy5) {
+            
+            console.log(i + " by 5");
+            
+            } else if (isBy3) {
+            
+            console.log(i + " by 3");
+            
+            } else {
+            
+            console.log(i);
+    }
+}
+
+}
+count(15); 
